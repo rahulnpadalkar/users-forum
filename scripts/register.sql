95e801a5c1d3c8ed4a31689b09214f6254781622
rahulnpadalkar <rahulnpadalkar@gmail.com>
2022-02-25 09:37:46 +0530

    Register returns a user

--- scripts/register.sql
+++ scripts/register.sql
@@ -5,9 +5,10 @@ CREATE OR REPLACE FUNCTION usersforumpublic.register_user(
     last_name TEXT
 ) RETURNS usersforumpublic.users AS $$
 DECLARE userid uuid;
+        reg_user users;
 BEGIN
-    INSERT INTO usersforumpublic.users(email, first_name, last_name) VALUES ($1, $3, $4) RETURNING id as userid;
-    INSERT INTO usersforumprivate.accounts(email, user_id, password) VALUES ($1, userid, crypt($2, gen_salt('bf')));
-    RETURN user;
+    INSERT INTO usersforumpublic.users(email, first_name, last_name) VALUES ($1, $3, $4) RETURNING * as reg_user;
+    INSERT INTO usersforumprivate.accounts(email, user_id, password) VALUES ($1, reg_user.id, crypt($2, gen_salt('bf')));
+    RETURN reg_user;
 END;
 $$ LANGUAGE PLPGSQL VOLATILE SECURITY DEFINER;
